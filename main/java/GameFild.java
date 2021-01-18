afaae282c10694e0fee7b6a89138d5913df40686
LittlesProgramer <magda.kocon1@op.pl>
2021-01-18 16:56:17 +0100

    update GameFild

--- main/java/GameFild.java
+++ main/java/GameFild.java
@@ -4,7 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class GameFild extends JFrame {
-    //private static Map<Integer,String> pozitionMarkMap = new HashMap<>();
+    private JComboBox<String> crossOrCircle = new JComboBox<String>(new String[]{"Cross","Circle"});
+    private JButton startGame = new JButton("Start Game");
+
+    private JPanel panelCrossCircle = new JPanel();
+    private JPanel panelButtons = new JPanel();
+
     private static Map<JButton,Integer> buttonMap = new HashMap<>();
 
     public GameFild(){
@@ -13,16 +18,34 @@ public class GameFild extends JFrame {
     }
 
     public void addTicTacToeFild(){
-        this.setLayout(new GridLayout(3,3));
+        this.setLayout(new BorderLayout());
+
+        this.add(panelCrossCircle,BorderLayout.NORTH);
+        panelCrossCircle.add(crossOrCircle);
+        panelCrossCircle.add(startGame);
+
+        this.add(panelButtons,BorderLayout.CENTER);
+        panelButtons.setLayout(new GridLayout(3,3));
+
         JButton fild = null;
+
         for(int x = 1 ; x <= 9 ; x++){
             fild = new JButton();
-            this.add(fild);
+            panelButtons.add(fild);
             buttonMap.put(fild,x);
         }
+
+        startGame.addActionListener((action)->{
+            System.out.println("cr = "+crossOrCircle.getItemAt(crossOrCircle.getSelectedIndex()));
+            crossOrCircle.setEnabled(false);
+        });
     }
 
     public static Map<JButton,Integer> getButtonMap() {
         return buttonMap;
+    }
+
+    public void userChooseCrossOrCircle(){
+
     }
 }

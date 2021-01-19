16a38cefb65e2d5db99a1cbb9e838f9b10bbbb54
LittlesProgramer <magda.kocon1@op.pl>
2021-01-19 11:17:08 +0100

    add comment to GameFild class

--- main/java/GameFild.java
+++ main/java/GameFild.java
@@ -5,19 +5,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class GameFild extends JFrame {
-    private JComboBox<String> crossOrCircle = new JComboBox<String>(new String[]{"Cross","Circle"});
-    private JButton startGame = new JButton("Start Game");
+    private JComboBox<String> crossOrCircle = new JComboBox<String>(new String[]{"Cross","Circle"}); //choose user
+    private JButton startGame = new JButton("Start Game"); //start game button
 
-    private JPanel panelCrossCircle = new JPanel();
-    private JPanel panelButtons = new JPanel();
+    private JPanel panelCrossCircle = new JPanel(); //pannel including user option choosing circle or cross
+    private JPanel panelButtons = new JPanel(); //panel containing 9 TicTacToe palying filds
+    private JPanel whoseMoveIsNow = new JPanel(); //this panel contains JLabel whoseMoveIsNowLabel
+    private JLabel whoseMoveIsNowLabel = new JLabel("Who move is now: "); //this label show whose move is now
 
-    private static Map<TicTacToeButtons,Integer> buttonMap = new HashMap<>();
+    private static Map<TicTacToeButtons,Integer> buttonMap = new HashMap<>(); //this map contains pairs - button and his location on the panelButtons
 
     public GameFild(){
         addTicTacToeFild();
-        GameLogicClass.UserMove();
+        GameLogicClass.UserMove(false);
     }
 
+    //this method add buttons,chooser panel cross or circle and show whose move is now
     public void addTicTacToeFild(){
         this.setLayout(new BorderLayout());
 
@@ -30,6 +33,7 @@ public class GameFild extends JFrame {
 
         TicTacToeButtons buttons = null;
 
+        //add 9 buttons to panelButtons
         for(int x = 1 ; x <= 9 ; x++){
             buttons = new TicTacToeButtons();
             buttons.setOpaque(false);
@@ -37,22 +41,32 @@ public class GameFild extends JFrame {
             buttonMap.put(buttons,x);
         }
 
+        //action for this button is choose circle or cross for user
         startGame.addActionListener((action)->{
             System.out.println("cr = "+crossOrCircle.getItemAt(crossOrCircle.getSelectedIndex()));
             crossOrCircle.setEnabled(false);
+            startGame.setEnabled(false);
         });
+
+        //add label who show whose move is now
+        this.add(whoseMoveIsNow,BorderLayout.SOUTH);
+        whoseMoveIsNow.setLayout(new FlowLayout(FlowLayout.LEFT));
+        whoseMoveIsNow.add(whoseMoveIsNowLabel);
+
     }
 
+    //this static method return buttonMap
     public static Map<TicTacToeButtons,Integer> getButtonMap() {
         return buttonMap;
     }
 
+    //this is class TicTacToe which draw circle or cross according to the user's choice using the drawingCircleOrCross method
     class TicTacToeButtons extends JButton{
         private Rectangle2D rect2D = new Rectangle2D.Double(10,10,10,10);
         private String figure = null;
 
         public void paintComponent(Graphics g){
-            System.out.println("paint");
+            //System.out.println("paint");
             if(figure != null) {
                 if (figure.equals("rect")) {
                     Graphics2D g2 = (Graphics2D) g;
@@ -63,7 +77,8 @@ public class GameFild extends JFrame {
             }
         }
 
-        public void rysuj(String s){
+        //according to this method choice is drawing circle or cross
+        public void drawingCircleOrCross(String s){
             figure = s;
         }
     }
--- main/java/GameLogicClass.java
+++ main/java/GameLogicClass.java
@@ -5,15 +5,22 @@ import java.util.Map;
 
 public class GameLogicClass {
     private static GameFild.TicTacToeButtons moveUser = null;
+    private static boolean doSelectedPlayer = false;
 
-    public static void UserMove(){
+    public static void UserMove(boolean selectedPlayer){
+        doSelectedPlayer = selectedPlayer;
+
         for(Map.Entry<GameFild.TicTacToeButtons,Integer> el : GameFild.getButtonMap().entrySet()){
             el.getKey().addActionListener((action)->{
-                System.out.println("id = "+el.getValue());
-                moveUser = el.getKey();
-                moveUser.setEnabled(false);
-                moveUser.setOpaque(false);
-                moveUser.rysuj("rect");
+                if(!doSelectedPlayer){
+                    JOptionPane.showMessageDialog(null,"Proszê wybraæ kó³ko lub krzy¿yk");
+                }else {
+                    System.out.println("id = " + el.getValue());
+                    moveUser = el.getKey();
+                    moveUser.setEnabled(false);
+                    moveUser.setOpaque(false);
+                    moveUser.drawingCircleOrCross("rect");
+                }
             });
         }
     }
